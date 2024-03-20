1faf0930395bd6c93ee8cca3cf8d7cd2abfc3080
Mikumikumi0116 <mikumikumi0116@gmail.com>
2024-03-20 13:49:17 +0800

    0.3.5 新动作，旋转摄像机到指定方向

--- C_Sharp_Backend/Action/Camera/Set_Camera_Rotation.cs
+++ C_Sharp_Backend/Action/Camera/Set_Camera_Rotation.cs
@@ -1,11 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-namespace C_Sharp_Backend.Action.Camera
-{
-    internal class Set_Camera_Rotation
-    {
+
+
+namespace Emulator_Backend{
+
+    public class Set_Camera_Rotation: Action_Base{
+        private CameraController controller = null;
+
+        public Set_Camera_Rotation() {
+            this.parameter_type_dict = new Dictionary<string, string>{
+                {"action",    "string"},
+                {"rot_pitch", "float"},
+                {"rot_yaw",   "float"}
+            };
+        }
+
+        public override Dictionary<string, object> Perform_action(Dictionary<string, object> action_dict){
+            if (!this.Check_parameter_validity(action_dict, out string parameter_validity_message)){
+                return new Dictionary<string, object> {
+                    {"status", "error"},
+                    {"message", parameter_validity_message}
+                };
+            }
+
+            float rot_pitch = (float)action_dict["rot_pitch"];
+            float rot_yaw   = (float)action_dict["rot_yaw"];
+
+            this.Set_camera_rotation_perform(rot_pitch, rot_yaw);
+
+            return new Dictionary<string, object> {
+                {"status",  "ok"},
+                {"message", "success"}
+            };
+        }
+
+        private void Set_camera_rotation_perform(float rot_pitch, float rot_yaw){
+            if (this.controller == null){
+                this.controller = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>();
+            }
+
+            var new_rot = new Vector2(rot_pitch, rot_yaw);
+            this.controller.m_targetAngle = new_rot;
+        }
     }
+
 }
--- Python_Frontend/test.py
+++ Python_Frontend/test.py
@@ -3,30 +3,67 @@ import requests
 
 
 
-# data = {
-#     "action": "Build_Straight_Road",
-#     "start_x": 600,
-#     "start_z": 100,
-#     "end_x": 0,
-#     "end_z": 0,
-#     "prefab_id": 89
-# }
-
-# data = {
-#     "action": "Create_Building",
-#     "pos_x": 0,
-#     "pos_z": 0,
-#     "angle": 3.14,
-#     "prefab_id": 1
-# }
-
-# data = {
-#     "action": "Get_Camera_Position",
-# }
+data = {
+    "action": "Build_Straight_Road",
+    "start_x": 600,
+    "start_z": 100,
+    "end_x": 0,
+    "end_z": 0,
+    "prefab_id": 89
+}
 
 data = {
-    "action": "Get_Camera_Rotation",
+    "action": "Create_Building",
+    "pos_x": 0,
+    "pos_z": 0,
+    "angle": 3.14,
+    "prefab_id": 1
 }
+
+data = {
+    "action": "Get_Camera_Position"
+}
+
+data = {
+    "action": "Get_Camera_Rotation"
+}
+
+data = {
+    "action": "Move_Camera",
+    "pos_x": 100,
+    "pos_y": 0,
+    "pos_z": 0,
+    "relative_to_camera": False
+}
+
+data = {
+    "action": "Move_Camera",
+    "pos_x": 100,
+    "pos_y": 0,
+    "pos_z": 0,
+    "relative_to_camera": True
+}
+
+data = {
+    "action": "Rotate_Camera",
+    "rot_pitch": 100,
+    "rot_yaw": 0
+}
+
+data = {
+    "action": "Set_Camera_Position",
+    "pos_x": 100,
+    "pos_y": 100,
+    "pos_z": 100
+}
+
+data = {
+    "action": "Set_Camera_Rotation",
+    "rot_pitch": 100,
+    "rot_yaw": 0
+}
+
+
 
 json_data = json.dumps(data)
 response = requests.post("http://localhost:11451/", data = json_data, headers = {"Content-Type": "application/json"})
