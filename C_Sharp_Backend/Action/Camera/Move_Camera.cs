3a09e31a3c2df84e3f790d60b9792ff0d5cebc80
Mikumikumi0116 <mikumikumi0116@gmail.com>
2024-03-20 13:40:46 +0800

    0.3.2 新动作，给定移动方向，基于当前位置移动摄像机，可以基于世界坐标系或摄像机本地坐标系

--- C_Sharp_Backend/Action/Camera/Move_Camera.cs
+++ C_Sharp_Backend/Action/Camera/Move_Camera.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 
 
-namespace C_Sharp_Backend.Action.Camera
-{
-    public class Move_Camera{
+namespace Emulator_Backend{
+
+    public class Move_Camera: Action_Base{
+        private CameraController controller = null;
+
+        public Move_Camera() {
+            this.parameter_type_dict = new Dictionary<string, string>{
+                {"action", "string"},
+                {"pos_x",  "float"},
+                {"pos_y",  "float"},
+                {"pos_z",  "float"},
+                {"relative_to_camera", "bool"}
+            };
+        }
+
+        public override Dictionary<string, object> Perform_action(Dictionary<string, object> action_dict){
+            if (!this.Check_parameter_validity(action_dict, out string parameter_validity_message)){
+                return new Dictionary<string, object> {
+                    {"status", "error"},
+                    {"message", parameter_validity_message}
+                };
+            }
+
+            float pos_x = (float)action_dict["pos_x"];
+            float pos_y = (float)action_dict["pos_y"];
+            float pos_z = (float)action_dict["pos_z"];
+            bool relative_to_camera = (bool)action_dict["relative_to_camera"];
+
+            this.Move_camera_perform(pos_x, pos_y, pos_z, relative_to_camera);
+
+            return new Dictionary<string, object> {
+                {"status",  "ok"},
+                {"message", "success"}
+            };
+        }
+
+        private void Move_camera_perform(float pos_x, float pos_y, float pos_z, bool relative_to_camera){
+            if (this.controller == null){
+                this.controller = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>();
+            }
+
+            var current_pos = this.controller.m_targetPosition;
+            var delta_pos   = new Vector3(pos_x, pos_y, pos_z);
+
+            Vector3 new_pos;
+            if (relative_to_camera){
+                var rotation  = this.controller.m_targetAngle;
+                var direction = Quaternion.Euler(rotation.x, rotation.y, 0);
+                new_pos       = current_pos + direction * delta_pos;
+            }
+            else {
+                new_pos = current_pos + delta_pos;
+            }
+
+            this.controller.m_targetPosition = new_pos;
+        }
     }
+
 }
